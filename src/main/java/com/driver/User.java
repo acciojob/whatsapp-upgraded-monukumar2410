41261f1ea6a53728d23a058922089c78d271c259
monukumar2410 <mrkumar.2410@gmail.com>
2023-01-29 13:44:29 +0530

    3

--- src/main/java/com/driver/User.java
+++ src/main/java/com/driver/User.java
@@ -4,6 +4,10 @@ public class User {
     private String name;
     private String mobile;
     
+    
+    public User() {
+    }
+    
     public User(String name, String mobile) {
         this.name = name;
         this.mobile = mobile;
--- src/main/java/com/driver/WhatsappService.java
+++ src/main/java/com/driver/WhatsappService.java
@@ -11,30 +11,30 @@ public class WhatsappService {
     WhatsappRepository whatsappRepository = new WhatsappRepository();
 
     public String createUser(String name, String mobile) throws Exception {
-        return createUser(name, mobile);
+        return whatsappRepository.createUser(name, mobile);
     }
 
     public Group createGroup(List<User> users){
-        return createGroup(users);
+        return whatsappRepository.createGroup(users);
     }
 
     public int createMessage(String content){
-        return createMessage(content);
+        return whatsappRepository.createMessage(content);
     }
 
     public int sendMessage(Message message, User sender, Group group) throws Exception{
-        return sendMessage(message, sender, group);
+        return whatsappRepository.sendMessage(message, sender, group);
     }
    
     public String changeAdmin(User approver, User user, Group group) throws Exception{
-        return changeAdmin(approver, user, group);
+        return whatsappRepository.changeAdmin(approver, user, group);
     }
 
     public int removeUser(User user) throws Exception{
-        return removeUser(user);
+        return whatsappRepository.removeUser(user);
     }
     public String findMessage(Date start, Date end, int K) throws Exception{
-        return findMessage(start,end, K);
+        return whatsappRepository.findMessage(start,end, K);
 
     }
 
